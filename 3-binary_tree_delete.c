9822356b1ded932c5d23cb0c52bbafc892439a0f
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-02-02 16:41:09 +0000

    fix eror

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - free an entire tree using recursion it free the node
+ * binary_tree_delete - free a tree with recursion free the node
  * when the node left and right are NULL
  * @tree: free tree
  * Return: none
