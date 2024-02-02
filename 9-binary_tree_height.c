90069559c220deeb9442fe9376dc36961254a0ae
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-02-02 20:36:44 +0000

    alx

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,8 +2,8 @@
 
 /**
  * binary_tree_height - it measures the height of a binary tree
- * @tree: tree to go through
- * Return: the height
+ * @tree: go through
+ * Return: Height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
