666163ddcc3d25c0f1cde5b78f3909e53817d476
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-02-02 15:43:10 +0000

    fix error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_insert_right - add a node in the right of the parent
  * if it exists it move down one level and add the new node first
- * @parent: parent of the specified node
+ * @parent: specified node parent
  * @value: value of the node
  * Return: NULL if it fails or the new node
  */
