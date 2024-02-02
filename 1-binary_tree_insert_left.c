3c5b6eb6beb22153a9402e7196337e350cae5c89
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-02-02 14:04:23 +0000

    fix error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,29 +2,29 @@
 /**
  * binary_tree_insert_left - add a node in the left of the parent
  * if it exists it move down one level and add the new node first
- * @parent: parent of the specified node
+ * @p: parent of the specified node
  * @value: value of the node
  * Return: NULL if it fails or the new node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *p, int value)
 {
 	binary_tree_t *newnd;
 
-	if (parent == NULL)
+	if (p == NULL)
 	{
 		return (NULL);
 	}
 
-	newnd = binary_tree_node(parent, value);
+	newnd = binary_tree_node(p, value);
 	if (newnd == NULL)
 	{
 		return (NULL);
 	}
-	if (parent->left != NULL)
+	if (p->left != NULL)
 	{
-		newnd->left = parent->left;
-		parent->left->parent = newnd;
+		newnd->left = p->left;
+		p->left->p = newnd;
 	}
-	parent->left = newnd;
+	p->left = newnd;
 	return (newnd);
 }
